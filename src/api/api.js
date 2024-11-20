03123b0cf8b3a4b45ddbbf7c6cfafefcf8654c02
Nasrullah Mansur <73380226+nasrullah-mansur@users.noreply.github.com>
2024-11-20 07:09:24 +0600

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://local-server-test.vercel.app",
+    baseURL: "http://localhost:5178:2000",
 });
 
 api.interceptors.request.use(
